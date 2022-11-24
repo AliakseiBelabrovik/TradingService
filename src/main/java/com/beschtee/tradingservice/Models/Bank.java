d0592c767894d4d31bdeb8b8960f8e2ab806f0c1
AliakseiBelabrovik <71708619+AliakseiBelabrovik@users.noreply.github.com>
2022-11-25 00:01:49 +0100

    Created basic structure for Spring Boot with Database Connection to PostgreSQL

---
+++ src/main/java/com/beschtee/tradingservice/Models/Bank.java
@@ -0,0 +1,31 @@
+package com.beschtee.tradingservice.Models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name="bank"
+)
+@NoArgsConstructor
+@Data
+public class Bank {
+    @Id
+    @SequenceGenerator(
+            name = "bank_id",
+            sequenceName = "bank_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "bank_id_seq"
+    )
+    @Column(name="id", updatable = false)
+    public Long id;
+
+    @Column( name="volume", nullable = false, columnDefinition = "NUMERIC(13,2) default 1000000000.00" )
+    public float volume;
+
+}
